490c4c461820b61099eedd4156f0f203ea5fb4e9
mchien15 <chien.lm204518@sis.hust.edu.vn>
2024-05-14 15:20:39 +0700

    update .gitignore

--- unwarping_test.sh
+++ unwarping_test.sh
@@ -8,6 +8,6 @@ output_folder="$2"
 
 for image_path in "$image_folder"/*; do
     if [ -f "$image_path" ]; then
-        ./test -idir="$image_path" -odir="$output_folder"
+        ./test_remap_INTER_CUBIC_BORDER_CONSTANT -idir="$image_path" -odir="$output_folder"
     fi
 done
